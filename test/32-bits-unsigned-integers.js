7431e9ce82cf8a199ad8e95f5fbb613add4cc0e5
Matteo Collina <matteo.collina@gmail.com>
2014-07-18 09:04:47 +0200

    Added 32 bits unsigned integers.

--- test/32-bits-unsigned-integers.js
+++ test/32-bits-unsigned-integers.js
@@ -2,30 +2,30 @@
 var test    = require('tap').test
   , msgpack = require('../')
 
-test('encoding/decoding 16-bits big-endian unsigned integers', function(t) {
+test('encoding/decoding 32-bits big-endian unsigned integers', function(t) {
   var encoder = msgpack()
     , allNum  = []
     , i
 
-  for (i = 256; i < 65536; i += 1423) {
+  for (i = 65536; i < 0xffffffff; i += 102350237) {
     allNum.push(i)
   }
 
-  allNum.push(65535)
+  allNum.push(65536 * 65536 - 1)
 
   allNum.forEach(function(num) {
     t.test('encoding ' + num, function(t) {
       var buf = encoder.encode(num)
-      t.equal(buf.length, 3, 'must have 3 bytes')
-      t.equal(buf[0], 0xcd, 'must have the proper header');
-      t.equal(buf.readUInt16BE(1), num, 'must decode correctly');
+      t.equal(buf.length, 5, 'must have 5 bytes')
+      t.equal(buf[0], 0xce, 'must have the proper header');
+      t.equal(buf.readUInt32BE(1), num, 'must decode correctly');
       t.end()
     })
 
     t.test('decoding ' + num, function(t) {
-      var buf = new Buffer(3)
-      buf[0] = 0xcd
-      buf.writeUInt16BE(num, 1)
+      var buf = new Buffer(5)
+      buf[0] = 0xce
+      buf.writeUInt32BE(num, 1)
       t.equal(encoder.decode(buf), num, 'must decode correctly');
       t.end()
     })
