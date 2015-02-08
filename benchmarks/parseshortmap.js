8d91a6ece19f3eefad6d68f1946397973b6dd13e
Matteo Collina <hello@matteocollina.com>
2015-02-08 09:48:36 +0000

    Added simple bench for pasing a small map.

---
+++ benchmarks/parseshortmap.js
@@ -0,0 +1,24 @@
+
+var msgpack = require('./')()
+  , bl      = require('bl')
+  , msg     = bl(msgpack.encode({ hello: 'world' }))
+  , decode  = msgpack.decode
+  , max     = 100000
+  , start
+  , stop
+  , i
+
+function run() {
+  for (i = 0; i < max; i++) {
+    decode(msg.duplicate())
+  }
+}
+
+//preheat
+run()
+
+start  = Date.now()
+run()
+stop = Date.now()
+console.log('time', stop - start)
+console.log('decode/s', max / (stop - start) * 1000)
