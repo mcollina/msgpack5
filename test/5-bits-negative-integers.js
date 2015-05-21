139dc84f5f47f863e849706142bf082bb2f580ad
Andrew Trimble <andrew.trimble@archarithms.com>
2015-05-21 12:40:24 -0500

    Ability to parse negative zero and added the test case.

--- test/5-bits-negative-integers.js
+++ test/5-bits-negative-integers.js
@@ -7,7 +7,7 @@ test('encoding/decoding 5-bits negative ints', function(t) {
     , allNum  = []
     , i
 
-  for (i = 1; i < 32; i++) {
+  for (i = 0; i < 32; i++) {
     allNum.push(-i)
   }
 
@@ -15,12 +15,12 @@ test('encoding/decoding 5-bits negative ints', function(t) {
     t.test('encoding ' + num, function(t) {
       var buf = encoder.encode(num)
       t.equal(buf.length, 1, 'must have 1 byte')
-      t.equal(buf[0], num + 0x100, 'must encode correctly');
+      t.equal(buf[0], Math.abs(num) + 0xe0, 'must encode correctly');
       t.end()
     })
 
-    t.test('decoding' + num, function(t) {
-      var buf = new Buffer([num + 0x100])
+    t.test('decoding ' + num, function(t) {
+      var buf = new Buffer([Math.abs(num) + 0xe0])
       t.equal(encoder.decode(buf), num, 'must decode correctly');
       t.end()
     })
