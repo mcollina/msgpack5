deece90f19621b3620e9877693001b2059053ba2
zheli-1 <zl3@pdx.edu>
2020-12-14 23:23:23 -0800

    unit test for date null

--- test/datenull.js
+++ test/datenull.js
@@ -2,11 +2,11 @@ var test = require('tape').test
 var msgpack = require('../')
 
 test('encode date is null ', function (t) {
-  var encoder =  msgpack({
+  var encoder = msgpack({
     disableTimestampEncoding: true
   })
 
-  t.equal(encoder.encode(null)[0], 0xc0 , 'encode null as null')
+  t.equal(encoder.encode(null)[0], 0xc0, 'encode null as null')
 
   t.end()
 })
