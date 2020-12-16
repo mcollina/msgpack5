0c293aff0d43594d2ca574c066fa8406d00039af
Zhe Li <32209826+zheli-1@users.noreply.github.com>
2020-12-16 08:45:02 -0800

    encode date is null fix (#85)
    * date is null fix
    
    * fix format
    
    * unit test for date null
    
    * unit test for date null
    
    * remove comment in code
    
    * add use strict
    
    * fix format

---
+++ test/datenull.js
@@ -0,0 +1,13 @@
+'use strict'
+var test = require('tape').test
+var msgpack = require('../')
+
+test('encode date is null ', function (t) {
+  var encoder = msgpack({
+    disableTimestampEncoding: true
+  })
+
+  t.equal(encoder.encode(null)[0], 0xc0, 'encode null as null')
+
+  t.end()
+})
