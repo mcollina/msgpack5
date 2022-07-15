c118cd38c7a18b136521090d61924b2303f2b994
cigui <cgcg96@sjtu.edu.cn>
2022-07-15 16:20:03 +0800

    fix: compatibility mode for bin format family (#110)

--- test/compatibility-mode.js
+++ test/compatibility-mode.js
@@ -3,6 +3,13 @@
 const test = require('tape').test
 const msgpack = require('../')
 
+function buildBuffer (size) {
+  const buf = Buffer.allocUnsafe(size)
+  buf.fill('a')
+
+  return buf
+}
+
 test('encode/compatibility mode', function (t) {
   const compatEncoder = msgpack({
     compatibilityMode: true
@@ -35,6 +42,32 @@ test('encode/compatibility mode', function (t) {
     const buf1 = defaultEncoder.encode(twoBytesStr)
     const buf2 = compatEncoder.encode(twoBytesStr)
     t.deepEqual(buf1, buf2, 'must be equal for two byte strings')
+    t.end()
+  })
+
+  const fixRawBuffer = buildBuffer(1)
+  const raw16Buffer = buildBuffer(Math.pow(2, 16) - 1)
+  const raw32Buffer = buildBuffer(Math.pow(2, 16) + 1)
+
+  t.test('compat. encoding a Buffer of length ' + fixRawBuffer.length, function (t) {
+    // fix raw header: 0xa0 | 1 = 0xa1
+    const buf = compatEncoder.encode(fixRawBuffer)
+    t.equal(buf[0], 0xa1, 'must have the proper header (fix raw)')
+    t.equal(buf.toString('utf8', 1, Buffer.byteLength(fixRawBuffer) + 1), fixRawBuffer.toString('utf8'), 'must decode correctly')
+    t.end()
+  })
+
+  t.test('compat. encoding a Buffer of length ' + raw16Buffer.length, function (t) {
+    const buf = compatEncoder.encode(raw16Buffer)
+    t.equal(buf[0], 0xda, 'must have the proper header (raw 16)')
+    t.equal(buf.toString('utf8', 3, Buffer.byteLength(raw16Buffer) + 3), raw16Buffer.toString('utf8'), 'must decode correctly')
+    t.end()
+  })
+
+  t.test('compat. encoding a Buffer of length ' + raw32Buffer.length, function (t) {
+    const buf = compatEncoder.encode(raw32Buffer)
+    t.equal(buf[0], 0xdb, 'must have the proper header (raw 32)')
+    t.equal(buf.toString('utf8', 5, Buffer.byteLength(raw32Buffer) + 5), raw32Buffer.toString('utf8'), 'must decode correctly')
     t.end()
   })
 })
